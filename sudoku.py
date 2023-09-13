f7c4b740e4afccb7374dd8a24ac3b7fe76ddcd1b
Tomasz Kluge <tomek@fakt.com.pl>
2023-09-13 13:03:45 +0200

    Made input into the sudoku validator

--- sudoku.py
+++ sudoku.py
@@ -1,22 +1,30 @@
-'''
+"""
 Sudoku validator
 
-'''
+"""
+sudoku = []
 
-sudoku1 = [
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [4, 5, 6, 7, 8, 9, 1, 2, 3],
-    [7, 8, 9, 1, 2, 3, 4, 5, 6],
-    [9, 1, 2, 3, 4, 5, 6, 7, 8],
-    [3, 4, 5, 6, 7, 8, 9, 1, 2],
-    [6, 7, 8, 9, 1, 2, 3, 4, 5],
-    [8, 9, 1, 2, 3, 4, 5, 6, 7],
-    [2, 3, 4, 5, 6, 7, 8, 9, 1],
-    [5, 6, 7, 8, 9, 1, 2, 3, 4],
-]
+for i in range(9):
+    row = []
+    for j in range(9):
+        while True:
+            value = input(f"Enter the number for row {i + 1}, column {j + 1}: ")
+            if value.isdigit() and 1 <= int(value) <= 9:
+                row.append(int(value))
+                break
+            else:
+                print("Please enter a valid number between 1 and 9.")
+    sudoku.append(row)
+
+
+def is_valid_set(lst):
+    return set(lst) == set(range(1, 10))
+
+
+valid = True
 
 # ROWS OPTION:
-for rows in sudoku1:
+for rows in sudoku:
     if sum(set(rows)) != 45:
         print(False)
         break
@@ -24,33 +32,31 @@ for rows in sudoku1:
 # COLUMNS:
 columns = []
 
-for i in sudoku1:
+for i in sudoku:
     for j in i:
         columns.append(j)
 
-if all(sum(set(columns[i : i + 73 : 9])) != 45 for i in range(9)):
+if all(sum(set(columns[i: i + 73: 9])) != 45 for i in range(9)):
     print(False)
 
 # SQUARE OPTION:
 one = []
-two= []
+two = []
 three = []
 
-for i in sudoku1:
+for i in sudoku:
     for j in i[:3]:
         one.append(j)
 
 first = one[:9], one[9:18], one[18:27]
 
-
-for i in sudoku1:
+for i in sudoku:
     for j in i[3:6]:
         two.append(j)
 
 second = two[:9], two[9:18], two[18:27]
 
-
-for i in sudoku1:
+for i in sudoku:
     for j in i[6:9]:
         three.append(j)
 
